f8399ed132b141cf40c035e0dd59cb4d859b00d9
SURAJPRABHA <surajprabha6363@gmail.com>
2026-05-07 19:16:52 +0530

    updated requirements

--- OVS/settings.py
+++ OVS/settings.py
@@ -22,10 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g$zt1v&9ord%aq3ed+xfk3ds1!k(e548k)f6c$5scakt3%0&l!'
+# SECRET_KEY = 'django-insecure-g$zt1v&9ord%aq3ed+xfk3ds1!k(e548k)f6c$5scakt3%0&l!'
+SECRET_KEY = os.environ.get('django-insecure-g$zt1v&9ord%aq3ed+xfk3ds1!k(e548k)f6c$5scakt3%0&l!')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = False
+DEBUG = os.environ.get('DEBUG') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
